76ad56b2596172f45afc2ee650b8a62ca106c9ef
ahu <1540303002@qq.com>
2020-04-02 16:25:43 +0800

    数据库1

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,12 +1,37 @@
+import os,sys
+
 from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy  
+import click
+
+
+WIN = sys.platform.startswith('win') # 判断是不是windows系统
+if WIN:
+    prefix = "sqlite:///" # window系统
+else:
+    prefix = "sqlite:////" # Mac linux系统
 
 app = Flask(__name__)
 
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     return "hello Flask"
+app.config['SQLALCHEMY_DATABASE_URI'] =  prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 对内存做优化
 
+db = SQLAlchemy(app) # 初始化app
+
+# models 数据层
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(20))
+    year = db.Column(db.String(4))
+
+
+
+
+# views 视图函数
 @app.route('/')
 def index():
     name = 'ahu'
@@ -17,3 +42,12 @@ def index():
         {'title':"战狼",'year':"2017"}
     ]
     return render_template('index.html',name=name,movie=movie)
+
+# 自定义命令
+@app.cli.command() # 注册为命令
+@click.option('--drop',is_flag=True,help="先删除在创建")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成！")
