0f425a348e7341e8a293cb730e9cc53576bcf1e8
ahu <1540303002@qq.com>
2020-04-13 10:32:10 +0800

    VUE组件

--- practise/src/App.vue
+++ practise/src/App.vue
@@ -1,40 +1,41 @@
 <template>
-<div class="con" id='app'>
+<div id="app">
   <h2>我的购物车清单</h2>
-  <div class='inp_shop'>
-      <input class='js-new' type="text" placeholder="输入你想购买的物品">
-      <span class='btu'>
-          <button class='js-add btn btn-default'>添加到购物车</button>
-      </span>
+  <div class="input-group">
+    <input class="js-new-item form-control" type="text" placeholder="输入你想购买的物品" />
+    <span class="input-group-btn">
+      <button type="button" class="js-add btn btn-default">添加到购物车</button>
+    </span>
   </div>
   <!-- 购物车列表 -->
   <ul>
-      <li>
-          <div class='checkbox'>
-              <label for="">
-                  <input class='js-item' type="checkbox">苹果
-              </label>
-          </div>
-      </li>
-      <li>
-          <div class='checkbox'>
-              <label for="">
-                  <input class='js-item' type="checkbox">香蕉
-              </label>
-          </div>
-      </li>
-      <li>
-          <div class='checkbox'>
-              <label for="">
-                  <input class='js-item' type="checkbox">榴莲
-              </label>
-          </div>
-      </li>
+    <li>
+      <div class="checkbox">
+        <label for>
+          <input type="checkbox" class="js-item" /> 苹果
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="checkbox">
+        <label for>
+          <input type="checkbox" class="js-item" /> 香蕉
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="removed">
+        <label for>
+          <input type="checkbox" class="js-item" /> 电脑
+        </label>
+      </div>
+    </li>
   </ul>
   <!-- 修改标题 -->
   <div class="footer">
-      <span>改变你的标题</span>
-      <input type="text" class='js-change' placeholder="我的购物车清单">
+    <hr />
+    <span>改变你的标题</span>
+    <input type="text" class="js-change-title" value="我的购物车清单" />
   </div>
 </div>
 </template>
@@ -43,21 +44,23 @@
 </script>
 
 <style>
-.con{
+.app {
   width: 40%;
   margin: 100px auto;
 }
-ul{
-  border: 1px solid black;
-  margin-top: 20px;
-}
-ul li{
-  list-style-type: none;
-}
-.removed{
+
+.removed {
   color: gray;
 }
-.removed label{
+
+.removed label {
   text-decoration: line-through;
+}
+ul {
+  border: 1px solid black;
+  margin-top: 30px;
+}
+ul li {
+  list-style: none;
 }
 </style>
